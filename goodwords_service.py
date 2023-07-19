cdadd6050d02f1a33dba7736f9b2ac092d13146f
Frank Isaacson <78625961+isaacson-f@users.noreply.github.com>
2023-07-19 18:04:14 -0400

    cleaned up code (#1)
    * cleaned up code
    
    * fix
    
    * removed exceptions
    
    * fixed logic
    
    * fixed historical words
    
    * find word checks
    
    * fix historical
    
    * fix
    
    * fixed lowercase
    
    * added lowercase

---
+++ goodwords_service.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timezone
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import logging
+import os
+from dotenv import load_dotenv
+import mongo_client
+from typing import Optional, List, Union
+
+
+client = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+good_words_collection = mongo_client.get_good_words_collection()
+
+def add_historical_goodwords():
+    # Call the conversations.list method using the WebClient
+    result = client.conversations_history(channel="C0441R6SKBN")
+    conversation_history = result["messages"]
+    for message in conversation_history:
+        word = message['text']
+        date_millis = float(message['ts'])
+        user_id = message['user']
+        temp_list = list(filter(lambda a: len(a) > 0, word.split(" ")))
+        if len(temp_list) == 1:
+            handle_word_sent(temp_list[0], date_millis, user_id, True)
+
+def process_event(event_obj: object):
+    event = event_obj['event']
+    if event.get('text', False) and event.get('ts', False) and event.get('user', False):
+        if event.get('subtype', False):
+            print(f"Only posts accepted; Received: {event.get('subtype')}")
+        message = event['text']
+        millis_time = float(event['ts'])
+        user = event['user']
+        temp_list = list(filter(lambda a: len(a) > 0, message.split(" ")))
+        if len(temp_list) > 1:
+            print(f"invalid submission: {temp_list}")
+        else: 
+            handle_word_sent(temp_list[0], millis_time, user)
+    else:
+        print(f"Event missing attribute ts or text: {event}")
+
+def handle_word_sent(word: str, millis_time: float, user_id: str, historical: bool=False):
+    prev_sent = find_word(word)
+    if prev_sent is not None:
+        if not historical:
+            client.chat_postMessage(channel="C0441R6SKBN", text=f"{word} was previously sent on {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}", thread_ts=str(millis_time))
+        print(f"Thread Time: {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}, Prev Sent Word: {word}")
+    elif not historical:
+        client.chat_postMessage(channel="C0441R6SKBN", text="Great Word :biting_lip:", thread_ts=str(millis_time))
+    else:
+        insert_new_word(word, millis_time, user_id)
+
+
+def insert_new_word(word: str, date_millis: float, user: str):
+    word_lowercase = word.lower()
+    document = {
+        "word": word_lowercase,
+        "date_millis": date_millis,
+        "user_id": user
+    }
+    good_words_collection.insert_one(document)
+    print(f"Successfully added word: \n {document['word']} \n millis: {document['date_millis']}")
+
+
+def find_word(word: str):
+    result = good_words_collection.find_one({"word": word.lower()})
+    print(f"Found: {result}")
+    return result
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timezone
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, status, Response
+from goodwords_service import add_historical_goodwords, process_event
 from pydantic import BaseModel
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import logging
 import os
 from dotenv import load_dotenv
-import MongoClient
+import mongo_client
 from typing import Optional, List, Union
 
 
@@ -14,98 +15,33 @@ load_dotenv()
 
 app = FastAPI()
 
-client = WebClient(token=os.environ.get("SLACK_TOKEN"))
-
-good_words_collection = MongoClient.get_good_words_collection()
-
 logger = logging.getLogger(__name__)
 
-class Reactions(BaseModel):
-    name: str
-    count: int
-    users: List[str]
-
-class SlackChallenge(BaseModel):
-    token: Optional[str]
-    challenge: Optional[str]
-    type: str
-    text: Optional[str]
-    channel: Optional[str]
-    user: Optional[str]
-    ts: Optional[float]
-    deleted_ts: Optional[float]
-    event_ts: Optional[float]
-    subtype: Optional[str]
-    hidden: Optional[bool]
-    is_starred: Optional[bool]
-    pinned_to: Optional[List[str]]
-    reactions: Optional[List[Reactions]]
+class HistoricalChannel(BaseModel):
+    channel_id: str
 
 @app.get("/")
 async def root():
     return ''
 
 
-@app.get("/history")
-async def root():
-    try:
-       # Call the conversations.list method using the WebClient
-        result = client.conversations_history(channel="C0441R6SKBN")
-        conversation_history = result["messages"]
-        for message in conversation_history:
-            word = message['text']
-            date_millis = float(message['ts'])
-            user_id = message['user']
-            temp_list = list(filter(lambda a: len(a) > 0, word.split(" ")))
-            if len(temp_list) > 1:
-               print(f"invalid submission: {temp_list}")
-            else:
-               if find_word(temp_list[0].lower()) is None:
-                   insert_new_word(temp_list[0].lower(), date_millis, user_id)
-    except SlackApiError as e:
-        print(f"Error: {e}")
-
+@app.post("/history", status_code=201)
+async def root(req: HistoricalChannel, response: Response):
+    if req.channel_id == "C0441R6SKBN":
+        add_historical_goodwords()
+    else:
+        response.status_code = 404
+    
 
 @app.post("/slack/events")
-async def root(resp: dict[str, object]):
-    if resp.get('challenge', False):
-        return resp['challenge']
-    elif resp.get('event', False):
-        event = resp['event']
-        if event.get('text', False) and event.get('ts', False) and event.get('user', False):
-            if event.get('subtype', False):
-                print(f"Only posts accepted; Received: {event.get('subtype')}")
-                return
-            message = event['text']
-            millis_time = float(event['ts'])
-            user = event['user']
-            temp_list = list(filter(lambda a: len(a) > 0, message.split(" ")))
-            if len(temp_list) > 1:
-               print(f"invalid submission: {temp_list}")
-            else: 
-                prev_sent = find_word(temp_list[0])
-                if prev_sent is not None:
-                    print(f"Thread Time: {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}, Prev Sent Word: {temp_list[0]}")
-                    client.chat_postMessage(channel="C0441R6SKBN", text=f"{temp_list[0]} was previously sent on {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}", thread_ts=str(millis_time))
-                else:
-                    insert_new_word(temp_list[0], millis_time, user)
-                    client.chat_postMessage(channel="C0441R6SKBN", text="Great Word :biting_lip:", thread_ts=str(millis_time))
-        else:
-            print(f"Event missing attribute ts or text: {event}")
+async def root(req: dict[str, object], resp: Response):
+    if req.get('challenge', False):
+        return req['challenge']
+    elif req.get('event', False):
+        process_event(req.get('event'))
     else:
-        print(f"Event missing attribute: {resp}")
+        resp.status_code = 400
+        print(f"Event missing attribute: {req}")
 
-def insert_new_word(word: str, date_millis: float, user: str):
-    document = {
-        "word": word,
-        "date_millis": date_millis,
-        "user_id": user
-    }
-    try:
-        good_words_collection.insert_one(document)
-        print(f"Successfully added word: \n {document['word']} \n millis: {document['date_millis']}")
-    except Exception as e:
-        print(f"Error adding word: {e}")
 
-def find_word(word: str):
-    return good_words_collection.find_one({"word": word})
+
