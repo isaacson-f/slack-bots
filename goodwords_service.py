d129eacae537e0c993e36df5353e1d96aed65ff0
Frank Isaacson <isaacson.f@northeastern.edu>
2023-07-19 16:22:35 -0400

    fix historical

--- goodwords_service.py
+++ goodwords_service.py
@@ -42,8 +42,9 @@ def process_event(event_obj: object):
 
 def handle_word_sent(word: str, millis_time: float, user_id: str, historical: bool=False):
     prev_sent = find_word(word)
-    if prev_sent is not None and not historical:
-        client.chat_postMessage(channel="C0441R6SKBN", text=f"{word} was previously sent on {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}", thread_ts=str(millis_time))
+    if prev_sent is not None:
+        if not historical:
+            client.chat_postMessage(channel="C0441R6SKBN", text=f"{word} was previously sent on {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}", thread_ts=str(millis_time))
         print(f"Thread Time: {datetime.fromtimestamp(prev_sent['date_millis']).strftime('%m/%d/%Y')}, Prev Sent Word: {word}")
     elif not historical:
         client.chat_postMessage(channel="C0441R6SKBN", text="Great Word :biting_lip:", thread_ts=str(millis_time))
